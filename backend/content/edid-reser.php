d4ed7698afccf9d8d68e0c4173e15e11f8be6742
JDM_David <fer.mtz.guz@gmail.com>
2024-12-02 08:33:23 -0600

    dasda

--- backend/content/edid-reser.php
+++ backend/content/edid-reser.php
@@ -92,11 +92,11 @@ if (!$reserva) {
         <form action="edit_reservation.php" method="POST">
             <input type="hidden" name="id" value="<?php echo htmlspecialchars($reserva['id_reserva']); ?>">
             <div class="mb-4">
-                <label for="fecha" class="block text-gray-200">Fecha:</label>
+                <label for="fecha" class="block text-[#333]-200">Fecha:</label>
                 <input type="date" name="fecha" id="fecha" value="<?php echo htmlspecialchars($reserva['fecha']); ?>" class="mt-1 block w-full p-2 rounded text-[#333]" required>
             </div>
             <div class="mb-4">
-                <label for="numero_personas" class="block text-gray-200">Número de Personas:</label>
+                <label for="numero_personas" class="block text-[#333]-200">Número de Personas:</label>
                 <input type="number" name="numero_personas" id="numero_personas" value="<?php echo htmlspecialchars($reserva['numero_personas']); ?>" class="mt-1 block w-full p-2 rounded text-[#333]" required>
             </div>
             <button type="submit" class="bg-blue-500 text-white p-2 rounded">Actualizar Reserva</button>
