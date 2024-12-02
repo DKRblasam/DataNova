698ef34fdc71abb9e5982db41ba2a9b35fd2daa8
JDM_David <fer.mtz.guz@gmail.com>
2024-12-02 01:28:41 -0600

    ...

--- backend/data/db.php
+++ backend/data/db.php
@@ -1,13 +1,39 @@
 <?php
-$host = 'tu_host';
-$db = 'tu_base_de_datos';
-$user = 'tu_usuario';
-$pass = 'tu_contraseña';
+// backend/databases/db.php
+
+$host = getenv('DB_HOST') ?: '10.177.135.248';
+$db   = getenv('DB_NAME') ?: 'RestaurantManagementDB';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: 'El.Pass_w0rd';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    // Log the error to a file or a centralized logging system
+    error_log("Database connection failed: " . $e->getMessage());
+    throw new \PDOException("Database connection failed.", 500);
 }
-?>
+
+try {
+    $sql = "SELECT * FROM clientes";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    while ($row = $stmt->fetch()) {
+        echo " <script> console.log ('
+        ID: {$row['id_cliente']} - Name: {$row['nombre']} - adrres: {$row['correo']} cel: {$row['telefono']} <br>'); </script>";
+    }
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./frontend/pages/**/*.html", 
+    "./frontend/pages/docs/**/*.html", 
+    "./frontend/src/**/*.{jgtml,css,js,jsx,ts,tsx}", // Si usas React o cualquier otro framework que genere JS
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
