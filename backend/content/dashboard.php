d5502ba611f6e772bbecb615070440ef259a0c70
Katan <fer.mtz.guz@gmail.com>
2024-12-03 07:22:10 -0600

    ...

--- backend/content/dashboard.php
+++ backend/content/dashboard.php
@@ -115,7 +115,7 @@ var_dump($user);  // Esto debería mostrar los detalles del usuario si la consul
         </div>
 
         <div class="text-center mt-6">
-            <a href="logout.php" class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Cerrar Sesión</a>
+            <a href="../../frontend/pages/" class="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Cerrar Sesión</a>
         </div>
     </div>
 </body>
