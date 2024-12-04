2bcd5df68b84049f5ef0fa2635c57a510ac7b119
Katan <fer.mtz.guz@gmail.com>
2024-12-04 06:35:55 -0600

    ..

--- backend/content/reservation.php
+++ backend/content/reservation.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Verificar si el usuario ha iniciado sesión
-if (!isset($_SESSION['user_id'])) {
+if (isset($_SESSION['user_id'])) {
     header("Location: login_register.php");
     exit();
 }
