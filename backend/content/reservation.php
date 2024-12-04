ac68b88dc9d7f1409822820c23e7583acb6a8b9b
Katan <fer.mtz.guz@gmail.com>
2024-12-04 06:30:23 -0600

    ..

--- backend/content/reservation.php
+++ backend/content/reservation.php
@@ -81,6 +81,10 @@ $reservas = $stmt->fetchAll();
         </nav>
     </header>
     <div class="container mx-auto p-4">
+        <a href="./reservaciones.php" style="text-decoration: none;">
+            Generar una nueva reservacion
+        </a>
+    <br><br>
         <h1 class="text-3xl font-bold text-center mb-4">Mis Reservas</h1>
 
         <table class="min-w-full bg-gray-800 rounded">
